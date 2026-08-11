3039870322bf62c936db0762fcd0a1f817a41bfd
Ajinkya <16738571+bar0meter@users.noreply.github.com>
2026-08-11 09:05:25 +0530

    .

---
+++ home/.config/fish/conf.d/git_ignore_tests.fish
@@ -0,0 +1,7 @@
+set -g __git_ignore_tests \
+    ':(exclude)*.test.js' ':(exclude)*.test.jsx' ':(exclude)*.test.ts' ':(exclude)*.test.tsx' \
+    ':(exclude)*.spec.js' ':(exclude)*.spec.jsx' ':(exclude)*.spec.ts' ':(exclude)*.spec.tsx' \
+    ':(exclude)*__tests__/*' ':(exclude)*tests/*' \
+    ':(exclude)*_test.go' \
+    ':(exclude)*_test.rs' \
+    ':(exclude)*Test.java' ':(exclude)*Tests.java' ':(exclude)*TestCase.java'
